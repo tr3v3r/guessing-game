33a683c5573cbd2203fdec03e5133f686354dd1f
tr3v3r <lochost40@gmail.com>
2017-01-28 10:09:16 +0300

    guessing-game:wrote in all methods

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    	this.start = null;
+    	this.end = null;
+    	this.middle = null;
+    }
 
     setRange(min, max) {
-
+    	this.start = min;
+    	this.end = max;
     }
 
     guess() {
-
+    	this.middle = Math.round((this.start + this.end)/2);
+    	return this.middle;
     }
 
     lower() {
-
+    	this.setRange(this.start,this.middle);
     }
 
     greater() {
-
+    	this.setRange(this.middle,this.end);
     }
 }
 
